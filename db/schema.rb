af83cc6fab1ca62b01079c62a4a879a83f46110b
Shingo miura <miugonis@icloud.com>
2017-08-05 12:58:03 +0000

    kadai-tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803082214) do
+ActiveRecord::Schema.define(version: 20170805124804) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
