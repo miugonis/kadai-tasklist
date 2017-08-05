60e9cbd4b4b7ca7496811ec7603163c48e182072
Shingo miura <miugonis@icloud.com>
2017-08-05 11:00:15 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,10 @@
 class TasksController < ApplicationController
-  
+  before_action :set_task, only: [:show, :edit, :update, :destroy]  
   def index
     @tasks = Task.all
   end
 
   def show  
-    set_task
   end
   
   def new
@@ -25,11 +24,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    set_task
   end
   
   def update
-    set_task
     
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -41,7 +38,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    set_task
     @task.destroy
     
     flash[:success] = 'Task は正常に削除されました'
