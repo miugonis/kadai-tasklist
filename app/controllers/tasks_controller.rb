3ab0e6f1f0777c969eee88d30c29341ee4a75446
Shingo miura <miugonis@icloud.com>
2017-08-20 13:04:53 +0000

    fix login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]  
+  before_action :require_user_logged_in, only: [:new, :create]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+
   def index
-    @tasks = Task.all.page(params[:page]).per(3)
-    @task = Task.new
+    if logged_in?
+      @tasks = current_user.tasks.page(params[:page]).per(3)
+    end
   end
 
   def show  
@@ -13,7 +17,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
@@ -49,6 +53,10 @@ class TasksController < ApplicationController
   
   def set_task
     @task = Task.find(params[:id])
+  end
+  
+  def correct_user
+    redirect_to root_url if @task.user != current_user
   end
 
   def task_params
